823b38d290d68995244be5c031630b7f627da075
swisa <d.swisa@f5.com>
2023-07-01 21:55:06 +0300

    fix: aaa

--- greeter/greeter_test.go
+++ greeter/greeter_test.go
@@ -2,12 +2,10 @@ package greeter
 
 import (
 	"testing"
-
-	"github.com/davidswisa/golang-with-bazel/greeter"
 )
 
 func TestGreeter(t *testing.T) {
-	got := greeter.Greet()
+	got := Greet()
 	want := "Hello, whoever!"
 	if got != want {
 		t.Errorf("Greet() = %q, want %q", got, want)
